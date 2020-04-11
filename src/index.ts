cdc895dfb0804f51ee77189411dd745b3da41d07
ldco2016 <renaissance.scholar2012@gmail.com>
2020-04-11 00:03:27 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { User } from "./User";
+// import { Company } from "./Company";
+// import { CustomMap } from "./CustomMap";
+
+const user = new User();
+
+console.log(user);
+// const company = new Company();
+// const customMap = new CustomMap("map");
+
+// customMap.addUserMarker(user);
+// customMap.addCompanyMarker(company);
